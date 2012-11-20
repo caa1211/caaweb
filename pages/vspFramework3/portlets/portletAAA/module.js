3c261e46dafefc00949b35d64fd25c4f7204c902
caa1211@gmail.com <caa1211@gmail.com@51281452-b6f2-11de-b71e-ab0a53e69959>
2012-11-20 11:40:44 +0000

    fix the bug: will the inner li of portlet will be the drop holder.

---
+++ pages/vspFramework3/portlets/portletAAA/module.js
@@ -0,0 +1,59 @@
+﻿
+//define(id?, dependencies?, factory); 
+define(function(require, exports, module){
+  var config = {};
+  
+  var initialize = function($el, config){
+  
+        //main function here=============================
+        var $view =$el;
+        var id = $view.id;
+     
+        $view.on("setting", function(e){
+            alert($view.id + " setting");
+        });
+        
+        $view.on("refresh", function(e){
+            alert($view.id + " refresh");
+        });
+  
+        $view.on("beforeDestory", function(e){
+            alert($view.id + " beforeDestory");
+        });
+        
+        $view.on("destroy", function(e){
+            //alert($view.id + " destroy");
+        });
+        
+        $view.on("fullScreenOn", function(e){
+            alert($view.id + " fullScreenOn");
+        });
+        
+        $view.on("fullScreenOff", function(e){
+            alert($view.id + " fullScreenOff");
+        });
+        /*
+		$("#"+id+"_btn").click(function(){
+			 alert($view.id + " button click");
+		});
+		*/
+		
+		$view.find('.myBtn').click(function(){
+			 //alert($view.id + " my button click");
+			 var newValue = $view.find('input.inputField').val();
+		     $view.trigger('updateConfig', {aaee: newValue});
+		});
+		
+        //public method
+        this.met = function(){ alert("met")}
+		
+	    //main function here=============================
+  };
+
+  return {
+    init: initialize
+  };
+  
+  
+});
+
